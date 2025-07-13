2d6773850fd636ec2e8c58675b9a4961cbf84ae5
Esraa <esraaelsaid2525@gmail.com>
2025-07-13 19:44:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import express from 'express'
 import sequelize from './config/database.js';
 import dotenv from 'dotenv';
-// import router from './routes/userRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import  setupAssociations from './models/associations.js';
-// dotenv.config();    
+
 const PORT = process.env.PORT || 2000;
 const app = express();
 
 setupAssociations()
 await sequelize.sync({alter: true}).then(()=>{
     console.log('Database connected successfully');
-}); // Sync the database with the models
+}); 
 app.use(express.json());
 
 app.get('/', (req, res) => {
